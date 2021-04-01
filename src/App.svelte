c86fcd769ff8642877d21e89f85b12cef02599d3
� Simone <simonetedesco.dev@gmail.com>
2021-04-01 23:59:24 +0200

    learning conditions with svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,29 @@
 <script>
 	let name = "prova";
 	let points = 100;
+	let showControls = false;
 
 	const addPoint = () => points += 1;
 	const removePoint = () => points -= 1;
+
+	const toggleControls = (params) => showControls = !showControls
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<h2>{points}</h2>
-	<button on:click={addPoint}>
-		+1
-	</button>
-	<button on:click={removePoint}>
-		-1
-	</button>
-	<input type="number" bind:value={points} />
+	<h2>Points: {points}</h2>
+	<div>
+		<button on:click={toggleControls}>{showControls ? "hide" : "show"} something</button>
+	</div>
+	{#if showControls}
+		<button on:click={addPoint}>
+			+1
+		</button>
+		<button on:click={removePoint}>
+			-1
+		</button>
+		<input type="number" bind:value={points} />
+	{/if}
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
