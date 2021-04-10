e455bdace615861c50f1c9c0c9b94e110091ced0
� Simone <simonetedesco.dev@gmail.com>
2021-04-10 23:45:50 +0200

    tried on:input

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,3 +1,12 @@
+<script>
+  let inputValue = "prova"
+  const handleInput = ({target}) => {
+    inputValue = target.value
+  }
+  
+</script>
+
 <nav class="navbar">
-  <h1>player scoreboard</h1>
+  <h1>player scoreboard {inputValue}</h1>
+  <input type="text" on:input={handleInput}>
 </nav>
