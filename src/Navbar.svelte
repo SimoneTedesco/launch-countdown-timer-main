a1051ece4ae5799137556198b5564c063f8fccbd
� Simone <simonetedesco.dev@gmail.com>
2021-04-10 23:49:48 +0200

    added 2 way binding

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -8,5 +8,9 @@
 
 <nav class="navbar">
   <h1>player scoreboard {inputValue}</h1>
-  <input type="text" on:input={handleInput}>
+  <!-- one way binding -->
+  <!-- <input type="text" on:input={handleInput}> -->
+  
+  <!-- two way binding + shorthand -->
+  <input type="text" bind:value={inputValue}>
 </nav>
