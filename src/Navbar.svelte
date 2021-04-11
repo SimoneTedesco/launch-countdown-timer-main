f6120e0eacd9223451456f85c16331a02f5fffff
� Simone <simonetedesco.dev@gmail.com>
2021-04-11 19:18:00 +0200

    tested reactive values

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -4,10 +4,22 @@
     inputValue = target.value
   }
   
+  // scrivendo così il valore non si aggiorna;
+  let reactiveValue = inputValue;
+  // invece così il valore si aggiorna, è una specie di watcher
+  $: reactiveValue = inputValue
+
+  // questo per fare piu cose contemporaneamente
+  $: {
+    console.log('reactiveValue :>> ', reactiveValue);
+    console.log('reactiveValue :>> ', reactiveValue);
+  }
+  
 </script>
 
 <nav class="navbar">
   <h1>player scoreboard {inputValue}</h1>
+  <p>{reactiveValue}</p>
   <!-- one way binding -->
   <!-- <input type="text" on:input={handleInput}> -->
   
