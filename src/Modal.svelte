16d707e49a69fce34de09b523b38b248558c56f0
� Simone <simonetedesco.dev@gmail.com>
2021-04-14 00:02:30 +0200

    conditional classes wow

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,12 +1,34 @@
 <script>
-  let showModal = false;
+  let showModal = true;
+  let isPromo = true;
 </script>
 
-<style></style>
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background-color: white;
+    color: #000;
+  }
+  .promo {
+    background-color: crimson;
+    color:white;
+  }
+</style>
 
 {#if showModal}
+<!-- class:promo aggiunge la classe "promo" condizionalmente -->
 <div class="backdrop">
-  <div class="modal">
+  <div class="modal" class:promo={isPromo}>
     <p>modal asd </p>
   </div>
 </div>
