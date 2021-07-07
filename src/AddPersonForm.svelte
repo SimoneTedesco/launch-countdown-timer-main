3bbbfe6ef37d1509258e3d796d14afe1bd196877
� Simone <simonetedesco.dev@gmail.com>
2021-07-07 23:50:42 +0200

    single checkbox

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,11 +2,17 @@
   let name;
   let beltColor;
   let age;
+  // let fighting = false;
+  // let sneaking = false;
+  // let running = false;
 
   const handleSubmit = () => {
     console.log('name :>> ', name);
     console.log('beltColor :>> ', beltColor);
     console.log('age :>> ', age);
+    // console.log('fighting :>> ', fighting);
+    // console.log('sneaking :>> ', sneaking);
+    // console.log('running :>> ', running);
   }
 </script>
 
@@ -15,6 +21,14 @@
   <input type="text" placeholder="2222" bind:value={beltColor}>
   <!-- svelte traduce in automatico input number in numero -->
   <input type="number" placeholder="age" bind:value={age}/>
+  <label for="">skills:</label>
+  <!-- metodo 1 checkbox singoli -->
+  <!-- <input type="checkbox" bind:checked={fighting}>fighting <br>
+  <input type="checkbox" bind:checked={sneaking}>sneaking <br>
+  <input type="checkbox" bind:checked={running}>running <br> -->
+  <!-- <input type="checkbox" bind:checked={fighting}>fighting <br>
+  <input type="checkbox" bind:checked={sneaking}>sneaking <br>
+  <input type="checkbox" bind:checked={running}>running <br> -->
   <button>add person</button>
 </form>
 
