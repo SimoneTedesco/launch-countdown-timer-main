e3b509b06a0e0030f9c619bc41ba8b430ca12c08
� Simone <simonetedesco.dev@gmail.com>
2021-07-08 00:07:11 +0200

    multiple checkbox

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -5,6 +5,7 @@
   // let fighting = false;
   // let sneaking = false;
   // let running = false;
+  // let skills = [];
 
   const handleSubmit = () => {
     console.log('name :>> ', name);
@@ -13,6 +14,7 @@
     // console.log('fighting :>> ', fighting);
     // console.log('sneaking :>> ', sneaking);
     // console.log('running :>> ', running);
+    // console.log('skills :>> ', skills);
   }
 </script>
 
@@ -22,13 +24,14 @@
   <!-- svelte traduce in automatico input number in numero -->
   <input type="number" placeholder="age" bind:value={age}/>
   <label for="">skills:</label>
-  <!-- metodo 1 checkbox singoli -->
+  <!-- checkbox singoli -->
   <!-- <input type="checkbox" bind:checked={fighting}>fighting <br>
   <input type="checkbox" bind:checked={sneaking}>sneaking <br>
   <input type="checkbox" bind:checked={running}>running <br> -->
-  <!-- <input type="checkbox" bind:checked={fighting}>fighting <br>
-  <input type="checkbox" bind:checked={sneaking}>sneaking <br>
-  <input type="checkbox" bind:checked={running}>running <br> -->
+  <!-- checkbox multipli -->
+  <!-- <input type="checkbox" bind:group={skills} value={"fighting"}>fighting <br>
+  <input type="checkbox" bind:group={skills} value={"sneaking"}>sneaking <br>
+  <input type="checkbox" bind:group={skills} value={"running"}>running <br> -->
   <button>add person</button>
 </form>
 
